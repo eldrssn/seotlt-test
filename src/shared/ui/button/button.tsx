6231c52af1d1d0cb07e25a06ec6401d162b9bb5f
Maxim Kalimullin <maxkalimullin@gmail.com>
2025-06-14 23:03:30 +0500

    fix register

--- src/shared/ui/button/button.tsx
+++ src/shared/ui/button/button.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'react';
 
 import { ButtonProps } from './types';
-import styles from './Button.module.scss';
+import styles from './button.module.scss';
 
 export const Button: FC<ButtonProps> = (props) => {
   const { children, variant = 'primary', className = '', ...rest } = props;
--- src/shared/ui/card/card.tsx
+++ src/shared/ui/card/card.tsx
@@ -1,7 +1,7 @@
 import { type FC } from 'react';
 
 import { CardProps } from './types';
-import styles from './Card.module.scss';
+import styles from './card.module.scss';
 
 export const Card: FC<CardProps> = ({ children, className = '' }) => {
   return <div className={`${styles.card} ${className}`}>{children}</div>;
